26d201a66c4306faa539142c032105e7d0168faf
Laurentiu Nicolae <nicolae_ovidiu_laurentiu@yahoo.com>
2018-02-11 19:07:31 +0200

    11.02.2018

---
+++ src/app/username.validators.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, ValidationErrors } from '@angular/core';
+
+export class UsernameValidators {
+
+  cannotContainSpace(control: AbstractControl) :ValidationError | null {
+    
+  }
+
+}
